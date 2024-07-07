48dd3bd5c9101d103be8b0df046f6176eeb4d315
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-07-07 10:40:45 +0200

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import ProductList from "./Components/ProductList/ProductList";
+import { Route, Routes } from "react-router-dom";
+import ProductDetails from "./Components/ProductDetails/ProductDetails";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ProductList />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/:id" element={<ProductDetails />} />
+      </Routes>
     </div>
   );
 }
